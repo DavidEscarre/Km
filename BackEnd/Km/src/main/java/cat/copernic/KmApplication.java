39ec82955a0b98c4d61d778e50674d64f3f6e568
alpep <david.degonz@gmail.com>
2025-03-20 12:30:46 +0100

    Estructura de Packages del frontend i backend

---
+++ BackEnd/Km/src/main/java/cat/copernic/KmApplication.java
@@ -0,0 +1,13 @@
+package cat.copernic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmApplication.class, args);
+	}
+
+}
